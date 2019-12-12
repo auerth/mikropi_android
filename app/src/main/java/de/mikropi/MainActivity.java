d351e8ea640f8f93ff825e11fd79b895ed3e297b
Thorben Auer <thorrogramm@gmail.com>
2019-12-12 08:29:22 +0100

    Fix PDF View

--- app/src/main/java/de/mikropi/MainActivity.java
+++ app/src/main/java/de/mikropi/MainActivity.java
@@ -1,11 +1,22 @@
 package de.mikropi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.text.format.DateUtils;
 import android.util.Log;
@@ -13,20 +24,31 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
+import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private ProgressBar spinner;
     private Button btnReload;
+    private static final int STORAGE_PERMISSION_CODE = 101;
+
 
     private final String TAG = "Mikropi-Log";
     private boolean doubleBackToExitPressedOnce = false;
@@ -34,10 +56,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                STORAGE_PERMISSION_CODE);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.webView);
-        spinner = (ProgressBar)findViewById(R.id.spinner);
-        btnReload = (Button)findViewById(R.id.reload);
+        spinner = (ProgressBar) findViewById(R.id.spinner);
+        btnReload = (Button) findViewById(R.id.reload);
 
         btnReload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,21 +76,30 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                spinner.setVisibility(View.VISIBLE);
-
-                view.loadUrl(url);
+                if(url.contains(".pdf")){
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(browserIntent);
+                }
+                    spinner.setVisibility(View.VISIBLE);
+                    view.loadUrl(url);
                 return false; // then it is not handled by default action
             }
+
             @Override
             public void onPageFinished(WebView view, String url) {
                 spinner.setVisibility(View.GONE);
                 webView.setVisibility(View.VISIBLE);
-            };
+            }
+
+            ;
+
             @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){
+            public void onReceivedError(WebView view,int errorCod,String description, String failingUrl) {
                 //Your code to do
-                webView.setVisibility(View.GONE);
-                btnReload.setVisibility(View.VISIBLE);
+                if(!failingUrl.contains(".pdf")) {
+                    webView.setVisibility(View.GONE);
+                    btnReload.setVisibility(View.VISIBLE);
+                }
 
             }
         });
@@ -74,13 +108,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onKeyLongPress(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
             Toast.makeText(this, "Long Press", Toast.LENGTH_SHORT).show();
             //Long Press code goes here
 
             return true;
         }
         return super.onKeyLongPress(keyCode, event);
+    }
+
+    private void pdfOpen(String fileUrl){
+
+
+
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(
+                WebView view, String url) {
+            return (false);
+        }
     }
 
     @Override
@@ -101,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void run() {
-                                doubleBackToExitPressedOnce=false;
+                                doubleBackToExitPressedOnce = false;
                             }
                         }, 2000);
                     } else {
@@ -114,22 +162,22 @@ public class MainActivity extends AppCompatActivity {
 
                     return true;
                 case KeyEvent.KEYCODE_VOLUME_DOWN:
-                        if (doubleBackToExitPressedOnce) {
-                            clearCache(this, 0);
-                            Toast.makeText(this, "Cache cleared", Toast.LENGTH_SHORT).show();
-                            return false;
-                        }
-                        this.doubleBackToExitPressedOnce = true;
-                        new Handler().postDelayed(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                doubleBackToExitPressedOnce=false;
-                            }
-                        }, 2000);
+                    if (doubleBackToExitPressedOnce) {
+                        clearCache(this, 0);
+                        Toast.makeText(this, "Cache cleared", Toast.LENGTH_SHORT).show();
+                        return false;
                     }
+                    this.doubleBackToExitPressedOnce = true;
+                    new Handler().postDelayed(new Runnable() {
 
-                    return true;
+                        @Override
+                        public void run() {
+                            doubleBackToExitPressedOnce = false;
+                        }
+                    }, 2000);
+            }
+
+            return true;
         }
         return super.onKeyDown(keyCode, event);
     }
@@ -178,6 +226,45 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, String.format("Starting cache prune, deleting files older than %d days", numDays));
         int numDeletedFiles = clearCacheFolder(context.getCacheDir(), numDays);
         Log.i(TAG, String.format("Cache pruning completed, %d files deleted", numDeletedFiles));
+    }
+
+    public void checkPermission(String permission, int requestCode) {
+
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(
+                MainActivity.this,
+                permission)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat
+                    .requestPermissions(
+                            MainActivity.this,
+                            new String[]{permission},
+                            requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super
+                .onRequestPermissionsResult(requestCode,
+                        permissions,
+                        grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this,
+                        "Storage Permission Granted",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                Toast.makeText(MainActivity.this,
+                        "Storage Permission Denied",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
     }
 
 }
